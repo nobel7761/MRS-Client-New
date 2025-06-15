409c296f5cdcc4df9e3f01bf921167407b2f8259
nobel912 <nobel@grype.ca>
2025-06-15 12:10:36 +0600

    column name changed

--- src/components/pages/admin/representative-registration/RepresentativeRegistration.component.tsx
+++ src/components/pages/admin/representative-registration/RepresentativeRegistration.component.tsx
@@ -111,7 +111,7 @@ const RepresentativeRegistrationComponent = () => {
     },
     {
       name: "facebookUrl",
-      label: "Facebook URL",
+      label: "Facebook",
       options: {
         filter: true,
         sort: true,
